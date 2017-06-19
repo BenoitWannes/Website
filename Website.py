493c69bc12d11a9fb9d145748b4fad4b59ebf343
wabenoit <wannes.benoit@student.howest.be>
2017-06-19 18:40:12 +0200

    test 3

--- Website.py
+++ Website.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
-from Website_Db import  DbClass
+from flask import Flask, render_template
+from Website_Db import DbClass
+import os
 
 app = Flask(__name__)
 
@@ -10,17 +11,18 @@ def index():
     return render_template('index.html')
 
 @app.route('/tabellen')
-#De tabellen met cerschillende gegevens
-def collection():
-    DB_layer = DbClass()
-    list_products = DB_layer.getcollection()
-    return render_template('collection.html', products=list_products)
+#De tabellen met verschillende gegevens
+def tabellen():
+    #DB_layer = DbClass()
+    #list_products = DB_layer.getcollection()
+    return render_template('tabellen.html')
 
 
 @app.errorhandler(404)
 def pagenotfound(error):
     return render_template("error.html", error =error)
 
-
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 8080))
+    host = "0.0.0.0"
+    app.run(host=host, port=port, debug=True)
